343fda5cce2877a0846f6a249c45c7a28769c390
Alvos3121 <OscarAlvarado@ciencias.unam.mx>
2020-07-26 19:32:56 -0500

    Create EspalexaBasic.ino
    Código básico para encendido / apagado de un foco. Funciona con NODEMCU

---
+++ EspalexaBasic/EspalexaBasic.ino
@@ -0,0 +1,110 @@
+/*
+ * This is a basic example on how to use Espalexa and its device declaration methods.
+ */ 
+#ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
+#include <Espalexa.h>
+
+#define D1 5 //GPIO 5 para la entrada D1
+
+// prototypes
+boolean connectWifi();
+
+//callback functions
+void firstLightChanged(uint8_t brightness);
+
+// Change this!!
+const char* ssid = "INFINITUMC3DD_2.4";
+const char* password = "5RxAGM8u4m";
+
+boolean wifiConnected = false;
+
+Espalexa espalexa;
+
+void setup()
+{
+  pinMode(D1, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(D1, LOW); // Por defecto lo ponemos como apagado. Ya que así se define desde EspAlexa
+  Serial.begin(115200);
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+  
+  if(wifiConnected){
+    
+    // Define your devices here. 
+    espalexa.addDevice("Foco 1", firstLightChanged); //simplest definition, default state off
+
+    espalexa.begin();
+    
+  } else
+  {
+    while (1) {
+      Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
+      delay(2500);
+    }
+  }
+}
+ 
+void loop()
+{
+   espalexa.loop();
+   delay(1);
+}
+
+//our callback functions
+void firstLightChanged(uint8_t brightness) {
+    Serial.print("Device 1 changed to ");
+    
+    //do what you need to do here
+
+    //EXAMPLE
+    if (brightness) {
+      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(D1, HIGH);
+      Serial.print("ON, brightness ");
+      Serial.println(brightness);
+    }
+    else  {
+      digitalWrite(LED_BUILTIN, HIGH);
+      digitalWrite(D1, LOW);
+      Serial.println("OFF");
+    }
+}
+
+// connect to wifi – returns true if successful or false if not
+boolean connectWifi(){
+  boolean state = true;
+  int i = 0;
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20){
+      state = false; break;
+    }
+    i++;
+  }
+  Serial.println("");
+  if (state){
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("Connection failed.");
+  }
+  return state;
+}
